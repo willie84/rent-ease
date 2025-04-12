60c39d5a3d64b4f73cb7bb51cc2efba50a6cb599
Willie Macharia <ngangawillie84@gmail.com>
2025-04-12 14:49:45 +0200

    Added first files

---
+++ src/main/java/com/example/rentease/RentEaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentEaseApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(RentEaseApplication.class, args);
+   }
+
+}
